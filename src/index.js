f5b791dd31792b1076af3e6c31c835aec31d9c6b
Isa(yunjongryu) <yunjonglyu@gmail.com>
2020-10-08 17:19:37 +0900

    feat: DEFAULT DIR STRUCTURE

--- src/index.js
+++ src/index.js
@@ -1,2 +1,23 @@
-import css from './css/main.css';
-import png from './images/test.png';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+/** redux */
+import { createStore } from 'redux';
+import reducers from './reducers'; 
+import { Provider } from 'react-redux';
+
+/** source */
+import App from './app';
+
+const store = createStore(reducers);
+
+const render = (Component) => {
+    ReactDOM.render(
+        <Provider store={store}>
+            <Component />
+        </Provider>,
+        document.getElementById('app')
+    );
+}
+
+render(App);
---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+
+
+const reducers = combineReducers({
+    // reducer
+});
+
+export default reducers;
