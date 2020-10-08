5d0f2f909336b3805c9d190add749789398e9eac
Isa(yunjongryu) <yunjonglyu@gmail.com>
2020-10-08 22:17:34 +0900

    feat: TODO MENU CONTROLLER - TODO ITEMS GET & SAVE

--- src/controller/todo.js
+++ src/controller/todo.js
@@ -1,14 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
+import todoModel from '../models/todoModel';
 
 import TodoModel from '../models/todoModel'; 
 
 import TodoView from '../view/todo';
 
 const Todo = () => {
-    const todoList = TodoModel();
+    const [todoList] = useState(TodoModel.getTodo);
+    const [todo, setToDo] = useState('');
+
+    const handleToDo = (e) => {
+      setToDo(e.target.value);
+    };
+    const handleAdd = (e) => {
+      e.preventDefault();
+      const result = {
+        "idx": todoList.length,
+        "todo" : todo
+      }
+
+      if(todo.split(' ').join('').length > 0){
+        todoList.push(result);
+        todoModel.saveTodo(todoList);
+        setToDo('');
+      }
+    };
+
     return (
-        <TodoView todoList={todoList}/>
+        <TodoView 
+        todoList = {todoList}
+        todoValue = {todo}
+        handleToDo = {handleToDo}
+        addToDo = {handleAdd}
+        />
     )
 };
 
