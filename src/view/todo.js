dfa6db546fb1cff321f9dafecc8f6f0c633e03aa
Isa(yunjongryu) <yunjonglyu@gmail.com>
2020-10-08 21:15:21 +0900

    feat: TODO MENU MARKUP

--- src/view/todo.js
+++ src/view/todo.js
@@ -1,10 +1,28 @@
 import React from 'react';
 
-const Todo = () => {
+import Layout from '../components/layout';
+
+const Todo = (props) => {
+    const { todoList } = props;
+    const todoItem = todoList.map((obj, index) => {
+        return (<li data-id={obj.idx} key={index}>{obj.todo}</li>)
+    });
+
     return (
-        <div>
-            todo
-        </div>
+        <Layout>
+            <div className="todo-contents">
+                <h2>Todo</h2>
+                <div className="todo-box">
+                    <ul className="todolist">
+                        {todoItem}
+                    </ul>
+                </div>
+                <form className="todo-from">
+                    <input type="text" />
+                    <button>+</button>
+                </form>
+            </div>
+        </Layout>
     )
 };
 
