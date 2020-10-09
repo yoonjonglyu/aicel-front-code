fc8f90b90700901864454702ccc12bfab50523bf
Isa(yunjongryu) <yunjonglyu@gmail.com>
2020-10-10 02:09:53 +0900

    feat: ASYNC MENU VIEW - BIND ASYNC DATA & EVENT

--- src/view/async.js
+++ src/view/async.js
@@ -2,28 +2,42 @@ import React from 'react';
 
 import Layout from '../components/layout';
 
-const async = () => {
+const async = (props) => {
+    const {
+        isRes,
+        asyncTarget,
+        asyncData,
+        handleCity
+    } = props;
+    
+    const asyncResult = isRes === true ? (
+        <div className="result-box">
+            <ul className="info">
+                <li>City : <span>{asyncData.city}</span></li>
+                <li>Weather : <span>{asyncData.weather}</span></li>
+                <li>Date : <time dateTime={asyncData.dateTime}>{asyncData.date}</time></li>
+            </ul>
+            <img src={asyncData.img} alt="weather image" />
+        </div>
+    ) : (
+        <div className="result-box">
+            <section className="loading">
+                Loading...
+            </section>
+        </div>
+    );
+        
     return (
         <Layout>
             <div className="async-contents">
                 <h2>Async Actions</h2>
                 <div className="options-box">
-                    <button>Seoul</button>
-                    <button>Tokyo</button>
-                    <button>Beijing</button>
-                    <button>Toronto</button>
+                    <button onClick={() => {handleCity(0)}}>Seoul</button>
+                    <button onClick={() => {handleCity(1)}}>Tokyo</button>
+                    <button onClick={() => {handleCity(2)}}>Beijing</button>
+                    <button onClick={() => {handleCity(3)}}>Toronto</button>
                 </div>
-                <div className="result-box">
-                    <section className="loading">
-                        Loading...
-                    </section>
-                    <ul className="info">
-                        <li>City : <span>Seoul</span></li>
-                        <li>Weather : <span>Clear</span></li>
-                        <li>Date : <time dateTime="YYYY-MM-DD">2020-10-08</time></li>
-                    </ul>
-                    <img src="" alt="weather image" />
-                </div>
+                {asyncResult}
             </div>
         </Layout>
     )
