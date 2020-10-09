8268f365cef5319f3fedb3ff64279c08b994aa87
Isa(yunjongryu) <yunjonglyu@gmail.com>
2020-10-09 16:57:17 +0900

    feat: COUNTER MENU VIEW - MARK UP

--- src/view/counter.js
+++ src/view/counter.js
@@ -1,10 +1,25 @@
 import React from 'react';
 
+import Layout from '../components/layout';
+
 const Counter = () => {
     return (
-        <div>
-            counter
-        </div>
+        <Layout>
+            <div className="counter-contents">
+                <h2>Counter</h2>
+                <div className="counter-box">
+                    <button>+</button>
+                    <strong>0</strong>
+                    <button>-</button>
+                </div>
+                <section className="title">
+                    <p>
+                        <span>Count title</span> :
+                        <strong className="title-name">waiting...</strong>
+                    </p>
+                </section>
+            </div>
+        </Layout>
     )
 };
 
