2e5ceaa20ee9bc808dd068b77efa0e7ce4bfee8a
Isa(yunjongryu) <yunjonglyu@gmail.com>
2020-10-09 13:03:54 +0900

    feat: GRID MENU CONTROLLER - SEARCH HANDLE

--- src/controller/grid.js
+++ src/controller/grid.js
@@ -1,4 +1,3 @@
-import { data } from 'autoprefixer';
 import React, { useState } from 'react';
 import { connect } from 'react-redux';
 
@@ -8,10 +7,23 @@ import GridView from '../view/grid';
 
 const Grid = () => {
     const [dataList, setDataList] = useState(GridModel.getData());
+    const [searchKeyword, setKeyword] = useState('');
+
+    const handleKeyword = (e) =>{
+        const keyword = e.target.value;
+        setKeyword(keyword);
+
+        if(keyword.length > 0){
+            const state = GridModel.searchData(GridModel.getData(), keyword);
+            setDataList(state);
+        }
+    }
 
     return (
         <GridView 
         dataTable = {dataList}
+        keyword = {searchKeyword}
+        handleKeyword = {handleKeyword}
         />
     )
 };
