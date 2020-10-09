40d17438cdf11e5a501081b8902367ac7866fb5e
Isa(yunjongryu) <yunjonglyu@gmail.com>
2020-10-09 14:35:18 +0900

    feat: MODALS MENU VIEW - MARKUP

--- src/view/modals.js
+++ src/view/modals.js
@@ -1,10 +1,26 @@
 import React from 'react';
 
+import Layout from '../components/layout';
+
 const Modals = () => {
     return (
-        <div>
-            modals
-        </div>
+        <Layout>
+            <div className="modals-contents">
+                <h2>Modals</h2>
+                <button>Show Modal</button>
+                <div className="modal-box">
+                    <section className="modal">
+                        <div className="modal-wrapper">
+                            <div className="modal-container">
+                                <h3>Modal Header</h3>
+                                <p>Modal Body</p>
+                                <button>OK</button>
+                            </div>
+                        </div>
+                    </section>
+                </div>
+            </div>
+        </Layout>
     )
 };
 
