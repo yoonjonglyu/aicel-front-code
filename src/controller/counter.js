2a2c462d0635ed8eae55fdd55c1de533050b5231
Isa(yunjongryu) <yunjonglyu@gmail.com>
2020-10-09 17:38:45 +0900

    feat: COUNTER CONTROLLER - HANDLE COUNT

--- src/controller/counter.js
+++ src/controller/counter.js
@@ -1,23 +1,44 @@
 import React, { useState } from 'react';
 import { connect } from 'react-redux';
+import * as actions from '../actions/index';
 
 import CounterModel from '../models/CounterModel'; 
 
 import CounterView from '../view/counter';
 
-const Counter = () => {
+const Counter = (props) => {
+    const {
+        count,
+        increment,
+        decrement
+    } = props;
+
+    const handleIncrement = () => {
+        increment();
+    }
+    const handleDecrement = () => {
+        decrement();
+    }
+
     return (
-        <CounterView />
+        <CounterView 
+        count = {count}
+        handleIncrement = {handleIncrement}
+        handleDecrement = {handleDecrement}
+        />
     )
 };
 
 const mapStateToProps = (state) => {
     return {
+        count : state.counter.counter
     }
   }
   
   const mapDispatchToProps = (dispatch) => {
     return {
+        increment : () => {dispatch(actions.INCREMENT())},
+        decrement : () => {dispatch(actions.DECREMENT())}
     }
   }
 
