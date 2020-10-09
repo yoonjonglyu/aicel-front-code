cf1b8d926689f233856578c03a4edd2aaffb0846
Isa(yunjongryu) <yunjonglyu@gmail.com>
2020-10-09 14:59:42 +0900

    feat: MODALS MENU CONTROLLER - HANDLE HIDE & SHOW

---
+++ src/controller/modals.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+
+import ModalsModel from '../models/ModalsModel'; 
+
+import ModalsView from '../view/Modals';
+
+const Modals = () => {
+    const [avail, setAvail] = useState(false);
+    const [modalHead, setHead] = useState('');
+    const [modalBody, setBody] = useState('');
+    
+    const handleShow = () => {
+        setAvail(true);
+    }
+    const handleHide = () => {
+        setAvail(false);
+    }
+
+    return (
+        <ModalsView 
+        isAvail = {avail}
+        headLine = {modalHead}
+        contents = {modalBody}
+        handleShow = {handleShow}
+        handleHide = {handleHide}
+        />
+    )
+};
+
+const mapStateToProps = (state) => {
+    return {
+    }
+  }
+  
+  const mapDispatchToProps = (dispatch) => {
+    return {
+    }
+  }
+
+export default connect(mapStateToProps, mapDispatchToProps)(Modals);
