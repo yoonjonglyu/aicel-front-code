f1957445f9cf0417932bb4af84fc82c1428dd5f7
Isa(yunjongryu) <yunjonglyu@gmail.com>
2020-10-09 11:13:22 +0900

    feat: GRID MENU VIEW - MARK UP

--- src/view/grid.js
+++ src/view/grid.js
@@ -1,10 +1,47 @@
 import React from 'react';
 
+import Layout from '../components/layout';
+
 const Grid = () => {
     return (
-        <div>
-            grid
-        </div>
+        <Layout>
+            <div className="grid-contents">
+                <h2>Grid</h2>
+                <div className="search-box">
+                    <input type="text" id="search" maxLength="10" placeholder="Please enter keywords" />
+                </div>
+                <div className="grid-box">
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>
+                                    name<span>-</span>
+                                </th>
+                                <th>
+                                    age<span>-</span>
+                                </th>
+                                <th>
+                                    weight<span>-</span>
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>
+                                    1
+                                </td>
+                                <td>
+                                    2
+                                </td>
+                                <td>
+                                    3
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </Layout>
     )
 };
 
