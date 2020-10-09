cf21ff6a837f483f559e8647a5a776e74f69523d
Isa(yunjongryu) <yunjonglyu@gmail.com>
2020-10-10 02:08:19 +0900

    feat: ASYNC MENU MODEL - GET API DATA & GET CITY

--- src/models/asyncModel.js
+++ src/models/asyncModel.js
@@ -1,4 +1,45 @@
 const AsyncModel = {};
 
+const getData = async (city) => {
+    const result = {};
+
+    try {
+        let state;
+        state = await fetch(`https://abnormal-weather-api.herokuapp.com/cities/search?city=${city}`);
+        state = await state.json();
+
+        if(!state.error){
+            result.city = state.city;
+            result.weather = state.current.weather;
+            result.datetime = state.current.date;
+            result.date = state.current.date.split('T')[0];
+            result.img = state.image_url;
+        }
+
+        return result;
+    } catch(e) {
+        console.error(e);
+        return result;
+    }
+}
+
+const getCity = (target) => {
+    const cityList = ["Seoul", "Tokyo", "Beijing", "Toronto"];
+    let index;
+
+    if(target > cityList.length){ // getter setter...;
+        index = (cityList.length - 1);
+    } else if(target < 0){
+        index = 0;
+    } else {
+        index = target ? target : 0;
+    }
+
+    const result = cityList[index];
+    return result;
+}
+
+AsyncModel.getData = getData;
+AsyncModel.getCity = getCity;
 
 export default AsyncModel;
