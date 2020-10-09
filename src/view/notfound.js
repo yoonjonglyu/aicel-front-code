004b8e2b9d56c635585ad25de5aaa3ecd957d237
Isa(yunjongryu) <yunjonglyu@gmail.com>
2020-10-10 02:15:09 +0900

    feat: NOT FOUND PAGE - MARK UP

--- src/view/notfound.js
+++ src/view/notfound.js
@@ -1,10 +1,18 @@
 import React from 'react';
 
+import Layout from '../components/layout';
+
 const NotFound = () => {
     return (
-        <div>
-            notFound
-        </div>
+        <Layout>
+            <div className="notfound-contents">
+                <h2>NotFound.</h2>
+                <hr />
+                <p>
+                    해당 페이지가 없습니다.<br />
+                </p>
+            </div>
+        </Layout>
     )
 };
 
