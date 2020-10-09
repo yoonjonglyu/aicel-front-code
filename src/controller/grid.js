893165e6a16f02933f7b173ac0d6dc4bf87b1548
Isa(yunjongryu) <yunjonglyu@gmail.com>
2020-10-09 14:21:28 +0900

    feat: GRID MENU CONTROLLER - HANDLE DATA SORT

--- src/controller/grid.js
+++ src/controller/grid.js
@@ -8,12 +8,29 @@ import GridView from '../view/grid';
 const Grid = () => {
     const [dataList, setDataList] = useState(GridModel.getData());
     const [searchKeyword, setKeyword] = useState('');
+    const [dataSort, setDataSort] = useState(GridModel.getSort());
 
-    const handleKeyword = (e) =>{
+    const handleKeyword = (e) => {
         const keyword = e.target.value;
         setKeyword(keyword);
 
         const state = GridModel.searchData(GridModel.getData(), keyword);
+        setDataList(state);
+        if(state.length > 0){
+            dataOrder(state);
+        }
+    }
+    const handleSort = (target) => {
+        const state = GridModel.getSort(dataSort);
+
+        state[target].isAvail = "active";
+        state[target].sort = GridModel.setOrder(state[target].sort);
+        setDataSort(state);
+        dataOrder(dataList);
+    }
+    const dataOrder = (data) => {
+        const state = GridModel.dataSort(data, dataSort);
+
         setDataList(state);
     }
 
@@ -22,6 +39,8 @@ const Grid = () => {
         dataTable = {dataList}
         keyword = {searchKeyword}
         handleKeyword = {handleKeyword}
+        tableSort = {dataSort}
+        handleSort = {handleSort}
         />
     )
 };
