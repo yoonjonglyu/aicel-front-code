75394c9f1689ee33aed9ad9ff9a63ea72939614b
Isa(yunjongryu) <yunjonglyu@gmail.com>
2020-10-10 20:10:36 +0900

    feat: FAVICON

--- build/webpack.common.js
+++ build/webpack.common.js
@@ -21,7 +21,7 @@ const config = {
     plugins : [
         new HtmlWebpackPlugin({
             template : 'public/index.html',
-            //favicon : 'public/favicon.ico'
+            favicon : 'public/ryufavicon.ico'
         }),
     ]
 }
